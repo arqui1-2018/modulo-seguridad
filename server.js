67e2158b0b920a784226bc429b582ef945f9aa63
Lester Santos <lestersantos271@gmail.com>
2018-08-31 14:38:56 -0600

    Add Server

--- server.js
+++
@@ -1,9 +0,0 @@
-var express require('express');
-var http = require('http');
-
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Modulo de seguridad interna corriendo...');
-    
-}).listen(process.env.PORT || 8080);
