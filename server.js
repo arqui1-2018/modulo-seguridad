ad2837473fb62329a856b1e86e8f46791fbc2189
201503440 <csazo171@gmail.com>
2018-08-30 19:20:21 -0600

    Cambio nombre

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Modulo de seguridad interga corriendo...');
+    res.end('Modulo de seguridad interna corriendo...');
     
 }).listen(process.env.PORT || 8080);
