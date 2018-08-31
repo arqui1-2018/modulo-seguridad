b9937aeaadc0b40b3b7aef1e6766b7b34df55eb7
201503440 <csazo171@gmail.com>
2018-08-30 18:38:26 -0600

    Cambios varios
    cambiar nombres

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world! <br> USING ARDUINO MEGA.!');
+    res.end('ARQUI 1 GRUPO 2');
     
 }).listen(process.env.PORT || 8080);
