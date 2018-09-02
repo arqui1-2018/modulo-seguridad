b2f3a62eb2d9fecb247b9348216454e5fe3e034b
201503440 <csazo171@gmail.com>
2018-09-02 08:08:43 -0600

    Metodo post

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # modulo-seguridad
+Universidad de San Carlos de Guatemala
+Facultad de Ingenieria
+Arqui 1
+Proyecto
+Grupo 2
 El modulo de seguridad con node js
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require('http');
+var bodyParser = require('body-parser');
 
 http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Modulo de seguridad interna corriendo...');
+    if (req.method == 'POST') {
+    	res.writeHead(200, { 'Content-Type': 'text/html' });
+    	res.end('SE HIZO PETICION POST AL SERVIDOR...');
+    }else{
+    	res.writeHead(200, { 'Content-Type': 'text/html' });
+    	res.end('Modulo de seguridad interna corriendo...');
+    }
     
 }).listen(process.env.PORT || 8080);
-//
